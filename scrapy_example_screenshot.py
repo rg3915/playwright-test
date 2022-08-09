c7a77de8d20f5b4c9a4955779fbcf4476449ccf8
rg3915 <regis42santos@gmail.com>
2022-08-09 00:27:18 -0300

    Add login and logout.

--- scrapy_example_screenshot.py
+++ scrapy_example_screenshot.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import sync_playwright
 
+# https://youtu.be/FK_5SQPq6nY
+
 with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page()
--- scrapy_quotes_toscrape.py
+++ scrapy_quotes_toscrape.py
@@ -1,8 +1,10 @@
 from playwright.sync_api import Playwright, sync_playwright
 
+# https://youtu.be/CK6MWehq7vI
+
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
+    browser = playwright.chromium.launch(headless=True)
     # browser = playwright.chromium.launch()  # running in background
     context = browser.new_context()
 
@@ -11,20 +13,32 @@ def run(playwright: Playwright) -> None:
 
     page.goto("http://quotes.toscrape.com/")
 
-    # Click text=Sign in
-    ua = page.query_selector('//div/div/div/div/span')
-    print(ua.inner_html())
-
     login = page.query_selector('[href="/login"]')
     login.click()
 
+    print('login...')
     username_input = page.query_selector('[id="username"]')
     username_input.type('lorem')
 
     password_input = page.query_selector('[id="password"]')
     password_input.type('admin123kkk')
 
-    page.wait_for_timeout(5000)
+    page.query_selector('input[type="submit"]').click()
+
+    selector = '//*[@href="/logout"]'
+    try:
+        print('Prepare to logout...')
+        page.wait_for_selector(selector, timeout=3000)
+        print('logouted...')
+    except:
+        print('login failed')
+        exit()
+
+    ua = page.query_selector('//div/div/div/div/span')
+    print(ua.inner_html())
+
+    print('Waiting more 2 seconds...')
+    page.wait_for_timeout(2000)
 
     # ---------------------
     context.close()
